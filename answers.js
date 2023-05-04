9a27dbc087dd7a4441cc1a2e559554dc33dec862
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-05-03 23:17:48 -0500

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -39,13 +39,30 @@ for (let i = 1; i < 201; i++){
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+//1.
+plantee[2] = 5001
 
+//2.
+wolfy[3] = "Gotham City"
+
+//3.
+dart[3] += " Hawkins"
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
+//4.
+wolfy[0] = "Gameboy"
 
-
+console.log(wolfy)
+console.log(sharky)
+console.log(plantee)
+console.log(dart)
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
