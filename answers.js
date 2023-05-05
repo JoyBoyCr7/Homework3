6bb2d40452c8976f79662761ea0c9ec20a85689f
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-05-05 15:30:44 -0500

    Methods Revisited answered

--- answers.js
+++ answers.js
@@ -51,7 +51,9 @@ plantee[2] = 5001
 wolfy[3] = "Gotham City"
 
 //3.
-dart[3] += " Hawkins"
+
+dart[3] += ", Hawkins"
+console.log(dart)
 wolfy[0] = "Gameboy"
 ////////////////////////////////
 // Yell at the Ninja Turtles
@@ -62,10 +64,61 @@ for (turtle in members){
     members[turtle] = members[turtle].toUpperCase()
 }
 console.log(members)
-
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+for (item in favMovies){
+    if (favMovies[item] === "Titanic"){
+        console.log(item)
+    }
+}
+//.1
+favMovies.sort()
+console.log(favMovies,"< question1")
+//.2
+favMovies.pop()
+//.3
+favMovies.push("Guardians of the Galaxy")
+//.4
+favMovies.reverse()
+//.5
+favMovies.shift()
+console.log(favMovies,"< question5")
+//6
+favMovies.unshift()
+console.log(favMovies,"< question6")
+// It doesnt do anything unless something is passed in
+
+//.7
+let index = 0
+for (item in favMovies){
+    if (favMovies[item] === "Django Unchained"){
+        index = item
+    }
+}
+favMovies.splice(index,1,"Avatar")
+console.log(favMovies)
+//.8 
+
+let newLen = favMovies.length / 2
+
+//.9
+let newSlice = favMovies.slice(0,newLen)
+console.log(newSlice,"< new array")
+
+//.10
+for (item in newSlice){
+    if (newSlice[item] ===  "Fast and Furious"){
+        console.log(item)
+    }  
+}
+
+function indexFinder(search){
+    return search == "Fast and Furious";
+}
+
+console.log(newSlice.findIndex(indexFinder))
 
 
 ////////////////////////////////
